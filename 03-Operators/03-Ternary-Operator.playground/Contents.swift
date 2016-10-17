beb6cce10f0766425bfc501a16c349da7631ea5c
liuyubobobo <liuyubobobo@gmail.com>
2016-10-17 18:07:06 +0800

    Chapter 03 code added.

--- 03-Operators/03-Ternary-Operator.playground/Contents.swift
+++ 03-Operators/03-Ternary-Operator.playground/Contents.swift
@@ -5,13 +5,15 @@ import UIKit
 var battery = 21
 let batteryColor:UIColor
 if battery <= 20{
-    batteryColor = UIColor.redColor()
+    batteryColor = UIColor.red
 }
 else{
-    batteryColor = UIColor.greenColor()
+    batteryColor = UIColor.green
 }
 batteryColor
 
 
-let batteryColor2 = battery <= 20 ? UIColor.redColor() : UIColor.greenColor()
+let batteryColor2 = battery <= 20 ? UIColor.red : UIColor.green
 
+
+// 在Swift3中，API名称的改变近乎是最大的一个改变。原先的UIColor.redColor()和UIColor.greenColor()变成了UIColor.red和UIColor.green，怎么样，是不是简单了很多？
